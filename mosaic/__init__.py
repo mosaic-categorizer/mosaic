8067b1994ef5f2bef1c69cae3297183fb5189852
Théo Jolivel <theo.jolivel@inria.fr>
2026-07-29 15:08:11 +0200

    add 'merge_tef_files_same_job' to default Mosaic import

--- mosaic/__init__.py
+++ mosaic/__init__.py
@@ -1,5 +1,5 @@
 from .categorizer import Categorizer
-from .tef_generators.darshan_to_tef import generate_traces_from_directory
+from .tef_generators.darshan_to_tef import generate_traces_from_directory, merge_tef_files_same_job
 from . import darshanv2logutils
 
-__all__ = ["Categorizer", "generate_traces_from_directory", "darshanv2logutils"]
+__all__ = ["Categorizer", "generate_traces_from_directory", "merge_tef_files_same_job", "darshanv2logutils"]
